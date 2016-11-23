5bd37383736591817ea40ef9f8c8230572740bef
forno96 <forno96@gmail.com>
2016-11-23 15:59:33 +0100

    0.2.0

--- Client.c
+++ Client.c
@@ -10,12 +10,23 @@
 #include <arpa/inet.h>
 
 int check(char d[]){
-    if(d[0]!='N'&'n'&'E'&'e'&'S'&'s'&'O'&'o'){
-        return(0);
-    } else {
-        return(1);
-    }
+    if((d[0]=='X')|(d[0]=='x')|(d[0]=='Y')|(d[0]=='y')){
+        if (d[1]=='('){
+            int i=2;
+            if (d[2]=='-') i=3;
+            if((d[i]=='1')|(d[i]=='2')|(d[i]=='3')|(d[i]=='4')|(d[i]=='5')|(d[i]=='6')|(d[i]=='7')|(d[i]=='8')|(d[i]=='9')|(d[i]=='0')){
+                i=i+1;
+                while ((d[i]=='1')|(d[i]=='2')|(d[i]=='3')|(d[i]=='4')|(d[i]=='5')|(d[i]=='6')|(d[i]=='7')|(d[i]=='8')|(d[i]=='9')|(d[i]=='0')) {
+                    i=i+1;
+                }
+                if ((d[i]==')')&(d[i+1]=='\0')){
+                    return 1;
+                } else return 0;
+            } else return 0;
+        } else return 0;
+    } else return 0;
 }
+
 int main(int argc, char* argv[]){
     struct sockaddr_in servaddr;
     int sockfd, errore;
@@ -38,31 +49,50 @@ int main(int argc, char* argv[]){
         
     }
     
-    char direzione[1];
-    int distanza;
-    /*
-    direzione[0]=' ';
+    //char direzione[512];
+    char* direzione = malloc(80*sizeof(char));
+    direzione[0]='\0';
     do{
-        if (direzione[0]!=' ') {
+        if (direzione[0]!='\0') {
             printf("Hai inserito una coordinata sbagliata!!\n");
         }
-        printf("Inserisci la direzione (N,E,S,O):");
-        scanf("%c",&direzione[0]);
-    } while(direzione[0]!='N'&'n'&'E'&'e'&'S'&'s'&'O'&'o');
-    */
+        printf("Inserisci la direzione (X(n) o Y (n)):");
+        scanf("%s", direzione);
+    } while(check(direzione)!=1);
     
-    printf("Inserisci la direzione (N,E,S,O):");
-    scanf("%c",&direzione[0]);
-    while (direzione[0]!='N'&'n'&'E'&'e'&'S'&'s'&'O'&'o'){
-        printf("Hai inserito un carattere errato!!!\nInserisci la direzione (N,E,S,O):");
-        scanf("%c",&direzione[0]);
+    /*
+    printf("Inserisci la direzione (X(n) o Y (n)):");
+    scanf("%s", direzione);
+    
+    int i=0;
+    while (direzione[i] != '\0') {
+        printf("%d:%c\n",i, direzione[i]);
+        i++;
     }
     
-    if ((write(sockfd,&direzione[0],1)) < 0 ) {
+    //printf("1: %c , %c\n", direzione[0], direzione[1]);
+    while (check(direzione)!=1){
+        printf("Hai inserito un carattere errato!!!\nInserisci la direzione (X(n) o Y (n)):");
+        scanf("%s", direzione);
+        i=0;
+        while (direzione[i] != '\0') {
+            printf("%d:%c\n",i, direzione[i]);
+            i++;
+        }
+    }
+    */
+    
+    int i=0;
+    while (direzione[i] != '\0') {
+        printf("%d:%c\n",i, direzione[i]);
+        i++;
+    }
+    
+    if ((write(sockfd,direzione,i)) < 0 ) {
         printf("Errore scrittura: %d\n",errore);
         exit (-3);
     }
-    
+
     /*
     char prova[20];
     scanf("%s",prova);
--- Server.c
+++ Server.c
@@ -48,8 +48,82 @@ void ChiudiSocket(int sock)
     return;
 }
 
-int main()
-{
+int char2int (char A[], int i){
+    if (A[i]=='1'){
+        return 1;
+    } else if (A[i]=='2'){
+        return 2;
+    } else if (A[i]=='3'){
+        return 3;
+    } else if (A[i]=='4'){
+        return 4;
+    } else if (A[i]=='5'){
+        return 5;
+    } else if (A[i]=='6'){
+        return 6;
+    } else if (A[i]=='7'){
+        return 7;
+    } else if (A[i]=='8'){
+        return 8;
+    } else if (A[i]=='9'){
+        return 9;
+    } else if (A[i]=='0'){
+        return 0;
+    } else {
+        printf("Prova7");
+        return -1;
+    }
+}
+
+int string2int(char A[],int i, int f){
+    printf("%d\n",f);
+    if (f>=i){
+        printf("Prova1\n");
+        if (f==i) {
+            int a=char2int(A,i);
+            if (a==-1) {
+                printf("Prova2\n");
+                return -1;
+            }
+            else {
+                printf("Prova3\n");
+                return a;
+            }
+        } else {
+            int b=char2int(A,f);
+            int c=string2int(A,i,f-1);
+            if ((b==-1)|(c==-1)) {
+                printf("Prova4\n");
+                return -1;
+            }
+            else {
+                printf("Prova5\n");
+                return (b+(c*10));
+            }
+        }
+    } else {
+        printf("Prova6\n");
+        return -1;
+    }
+}
+
+int gapArray(char A[]){
+    if(((A[0]=='X')|(A[0]=='x')|(A[0]=='Y')|(A[0]=='y'))&(A[1]=='(')){
+        for(int i=2; i<512; i=i+1){
+            printf("Prova11 %d\n",i);
+            if (A[i]==')') {
+                printf("Prova8\n");
+                return (i-1);
+            }
+        }
+        printf("Prova9\n");
+        return -1;
+    }
+    printf("Prova10\n");
+    return -1;
+}
+
+int main(){
     //N.B. L'esempio non usa la funzione fork per far vedere l'utilizzo di
     //     socket non bloccanti
     
@@ -58,7 +132,6 @@ int main()
     int exitCond=0;
     int Quanti, Quanti2;
     int x=0, y=0;
-    int distanza;
     DescrittoreSocket=CreaSocket(1745);
     printf("Server: coordinate attuali:[%d,%d] \n",x,y);
     printf("Server: Attendo connessioni...\n");
@@ -78,18 +151,26 @@ int main()
                 //Aggiusto la lunghezza...
                 buffer[Quanti]=0;
                 //Elaborazione dati ricevuti
-                if (buffer[0]=='N'|buffer[0]=='n') {
-                    printf("X icrementata di %d\n",1);
-                    x=x+1;
-                } else if (buffer[0]=='E'|buffer[0]=='e') {
-                    printf("Y icrementata di %d\n",1);
-                    y=y+1;
-                } else if (buffer[0]=='S'|buffer[0]=='s') {
-                    printf("X diminuita di %d\n",1);
-                    x=x-1;
-                } else if (buffer[0]=='O'|buffer[0]=='o') {
-                    printf("Y diminuita di %d\n",1);
-                    y=y-1;
+                for(int e=0;e<5;e=e+1){
+                    printf("%d:%c\n",e,buffer[e]);
+                }
+                printf("\n");
+                
+                int i;
+                if (buffer[2]=='-') {
+                    i=-(string2int(buffer,3,gapArray(buffer)));
+                    if (i==1) return 1;
+                } else {
+                    i=string2int(buffer,2,gapArray(buffer));
+                    if (i==-1) return 1;
+                }
+                
+                if (buffer[0]=='X'|buffer[0]=='x') {
+                    printf("X modificata di %d\n",i);
+                    x=x+i;
+                } else if (buffer[0]=='Y'|buffer[0]=='y') {
+                    printf("Y modificata di %d\n",i);
+                    y=y+i;
                 } else {
                     printf("Server: Imput Error \n");
                 }
@@ -109,6 +190,5 @@ int main()
     //Chiusura del socket
     ChiudiSocket(DescrittoreSocket);
     printf("Server: Terminato.\n");
-    
     return 0;
 }
