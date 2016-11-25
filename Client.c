9f6d3afbe6e199b7bc4ecf864adf8a239e225e8c
GFdevelop <gabriele.fulgaro@gmail.com>
2016-11-25 17:18:22 +0100

    my patch

--- Client.c
+++ Client.c
@@ -1,108 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <fcntl.h>
+//#include <sys/types.h>
+//#include <netinet/in.h>
+//#include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
+//#include <netdb.h>
 #include <arpa/inet.h>
 
-int check(char d[]){
-    int i = 2;
-    if((d[0]!='S')&(d[0]!='s')&(d[0]!='D')&(d[0]!='d')&(d[0]!='A')&(d[0]!='a')&(d[0]!='B')&(d[0]!='b')) return 0;
-    //printf("1\n");
-    if (d[1]!='(') return 0;
-    //printf("2\n");
-    if (d[2]=='-') {i=3;/*printf("3\n");*/}
-    while ((d[i]=='1')|(d[i]=='2')|(d[i]=='3')|(d[i]=='4')|(d[i]=='5')|(d[i]=='6')|(d[i]=='7')|(d[i]=='8')|(d[i]=='9')|(d[i]=='0')) {i=i+1;/*printf("4\n");*/}
-    //printf("5\n");
-    if ((i==2) | ((i==3)&((d[2])=='-'))) return 0;
-    //printf("6\n");
-    if (d[i]!=')') return 0;
-    //printf("7\n");
-    if ((d[i+1]!='\0')&(d[i+1]!='\n')&(d[i+1]!='\r')) return 0;
-    //printf("8\n");
-    return 1;
-}
+#define MAXLINE	512
 
 int main(int argc, char* argv[]){
-    struct sockaddr_in servaddr;
-    int sockfd, errore;
-    
-    sockfd=socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0 ) {
-        printf("Errore socket: %d\n",sockfd);
-        exit (-1);
-    }
-    
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_port = htons(atoi(argv[2]));
-    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
-    
-    errore = connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-    if (errore < 0 ) {
-        printf("Errore connessione: %d\n",errore);
-        exit (-2);
-        
-    }
-    
-    //input direzione
-    //char* direzione = malloc(80*sizeof(char));
-    printf("Prova1\n");
-    char * direzione;
-    printf("Prova2\n");
-    int cilclo=1;
-    do{
-        if (cilclo!=1) {
-            printf("Hai dato un input non corretto!!\n");
-        }
-        printf("Inserisci la direzione (A(n),B(n),S(n),D(n)):");
-        scanf("%ms",&direzione);
-        cilclo++;
-    } while(check(direzione)!=1);
-    
-    int i=0;
-    while (direzione[i] != '\0') {
-        i++;
-    }
-    
-    //invio direzione
-    if ((write(sockfd,direzione,i)) < 0 ) {
-        printf("Errore scrittura: %d\n",errore);
-        exit (-3);
-    }
-    
-    /*
-    // ritorno del messaggio
-    char  buffer[512];
-    if (read(socket,buffer,sizeof(buffer))>0){
-        printf("Posizione puntatore: %s",buffer);
-    } else (n<0) {
-        printf("Messaggio non ritornato");
-    }
-    */
-    
-    //ripetizione
-    char flag[1]={'\0'};
-    int f=0;
-    do{
-        if (flag[0]!='\0') printf("%s non è una risposta corretta\n",&flag[0]);
-        printf("Vuoi continuare? (Y o N):");
-        scanf("%s",&flag[0]);
-        f=0;
-        while (flag[f] != '\0') {
-            f++;
-        }
-    } while (((f!=1))|((flag[0]!='Y')&(flag[0]!='y')&(flag[0]!='N')&(flag[0]!='n')));
-    
-    if ((flag[0]=='Y')||(flag[0]=='y')) {
-        main(argc,argv);
-    }
-    free (direzione);
-    close(sockfd);
-    
-    return 0;
+	struct sockaddr_in servaddr;
+	int sockfd;
+	
+	sockfd=socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0 ) {
+		printf("Errore socket: %d\n",sockfd);
+		exit (-1);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_port = htons(atoi(argv[2]));
+	servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+	
+	if (connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0){
+		printf("Errore connessione\n");
+		exit (-2);
+	}
+	
+	char direzione[MAXLINE];
+	scanf("%[^\t\n]",direzione);
+	
+	if (direzione == NULL) {
+		printf("errore\n");
+		exit (-5);
+	}
+	
+	/*if (check(direzione)==0) {
+		printf("Error -4");
+		exit (-4);
+	}*/
+	
+	
+	if (write(sockfd,direzione,strlen(direzione)) < 0){
+		printf("Errore scrittura\n");
+		exit (-3);
+	}
+
+	close(sockfd);
+	
+	return 0;
 }
--- Server.c
+++ Server.c
@@ -1,244 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <fcntl.h>
+//#include <sys/types.h>
+//#include <netinet/in.h>
+//#include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+//#include <netdb.h>
 #include <arpa/inet.h>
 
+#define MAXLINE	512
 
-#define MAXLINE 99
-
-#include <sys/types.h>
-#include <sys/socket.h>
-//"in" per "sockaddr_in"
-#include <netinet/in.h>
-//"fcntl" per la funzione "fcntl"
-#include <fcntl.h>
-
-int CreaSocket(int Porta){
-    int sock,errore;
-    struct sockaddr_in temp;
-    
-    //Creazione socket
-    sock=socket(AF_INET,SOCK_STREAM,0);
-    //Tipo di indirizzo
-    temp.sin_family=AF_INET;
-    temp.sin_addr.s_addr=INADDR_ANY;
-    temp.sin_port=htons(Porta);
-    
-    //Il socket deve essere non bloccante
-    errore=fcntl(sock,F_SETFL,O_NONBLOCK);
-    
-    //Bind del socket
-    errore=bind(sock,(struct sockaddr*) &temp,sizeof(temp));
-    //Per esempio, facciamo accettare fino a 7 richieste di servizio
-    //contemporanee (che finiranno nella coda delle connessioni).
-    errore=listen(sock,7);
-    
-    return sock;
+int check(char d[]){
+	int i = 2;
+	
+	if (d==NULL) return 0;
+    if((d[0]!='S')&(d[0]!='s')&(d[0]!='D')&(d[0]!='d')&(d[0]!='A')&(d[0]!='a')&(d[0]!='B')&(d[0]!='b')) return 0;
+	if (d[1]!='(') return 0;
+	if (d[2]=='-') i=3;
+	while ((d[i]=='1')|(d[i]=='2')|(d[i]=='3')|(d[i]=='4')|(d[i]=='5')|(d[i]=='6')|(d[i]=='7')|(d[i]=='8')|(d[i]=='9')|(d[i]=='0'))
+		i=i+1;
+	if ((i==2)|((i==3) & ((i-1)=='-'))) return 0;
+	if (d[i]!=')') return 0;
+	if ((d[i+1]!='\0')&(d[i+1]!='\n')&(d[i+1]!='\r')) return 0;
+	return 1;
 }
 
-void ChiudiSocket(int sock){
-    close(sock);
-    return;
-}
+int main(int argc, char* argv[]){
+	struct sockaddr_in servaddr;
+	int listenfd, connfd, errore, lung;
+	char buf[MAXLINE];
+	
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+		printf("%d\n",listenfd);
 
-int char2int (char A[], int i){
-    if (A[i]=='1'){
-        return 1;
-    } else if (A[i]=='2'){
-        return 2;
-    } else if (A[i]=='3'){
-        return 3;
-    } else if (A[i]=='4'){
-        return 4;
-    } else if (A[i]=='5'){
-        return 5;
-    } else if (A[i]=='6'){
-        return 6;
-    } else if (A[i]=='7'){
-        return 7;
-    } else if (A[i]=='8'){
-        return 8;
-    } else if (A[i]=='9'){
-        return 9;
-    } else if (A[i]=='0'){
-        return 0;
-    } else {
-        return -1;
-    }
-}
+	memset(&servaddr,0 , sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(atoi(argv[1]));	/* daytime server */
+	printf("local address: IP %s port %d\n", inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port) );
 
-char int2char(int i){
-    if (i==1){
-        return '1';
-    } else if (i==2){
-        return '2';
-    } else if (i==3){
-        return '3';
-    } else if (i==4){
-        return '4';
-    } else if (i==5){
-        return '5';
-    } else if (i==6){
-        return '6';
-    } else if (i==7){
-        return '7';
-    } else if (i==8){
-        return '8';
-    } else if (i==9){
-        return '9';
-    } else if (i==0){
-        return '0';
-    } else {
-        return '\0';
-    }
-}
+	errore = bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+		printf("%d\n",errore);
+	
+	errore = listen(listenfd, 10);
+		printf("%d\n",errore);
 
-int string2int(char A[],int i, int f){
-    if (f>=i){
-        if (f==i) {
-            int a=char2int(A,i);
-            if (a==-1) return -1;
-            else return a;
-        } else {
-            int b=char2int(A,f);
-            int c=string2int(A,i,f-1);
-            if ((b==-1)|(c==-1)) return -1;
-            else return (b+(c*10));
-        }
-    } else return -1;
-}
-
-int gapArray(char A[]){
-    if(((A[0]=='A')|(A[0]=='a')|(A[0]=='B')|(A[0]=='b')|(A[0]=='D')|(A[0]=='d')|(A[0]=='S')|(A[0]=='s'))&(A[1]=='(')){
-        for(int i=2; i<512; i=i+1){
-            if (A[i]==')') {
-                return (i-1);
-            }
-        }
-        return -1;
-    }
-    return -1;
-}
-
-int createChar(char A[],int x, int y, int select){
-    if (select==0){
-        A[0]='[';
-        int i=1,a;
-        i=createChar(A, x, i, 1);
-        createChar(A, 1, i-1, 2);
-        A[i]=',';
-        i++;
-        a=createChar(A, y, i, 1);
-        createChar(A, i, a-1, 2);
-        A[a]=']';
-        A[a+1]='\0';
-    } else if (select==1){
-        int i=y,e,a=x;
-        if (x<0) {a=-x; A[y]='-'; i++;}
-        while (a>0) {
-            e=a%10;
-            a=(a-e)/10;
-            A[i]=int2char(e);
-            i++;
-        }
-        return i;
-    } else if (select==2){
-        int a=x,b=y,e;
-        if (A[x]=='-') a=x+1;
-        while (a<b) {
-            e=A[b];
-            A[b]=A[a];
-            A[a]=e;
-            b--; a++;
-        }
-    }
-    return 0;
-}
-
-int main(){
-    //N.B. L'esempio non usa la funzione fork per far vedere l'utilizzo di
-    //     socket non bloccanti
-    
-    char  buffer[512];
-    
-    char buffer2[512];
-    int buf=0;
-    
-    int DescrittoreSocket,NuovoSocket;
-    int exitCond=0;
-    int Quanti, Quanti2;
-    int x=0, y=0;
-    DescrittoreSocket=CreaSocket(1745);
-    printf("Server: coordinate attuali:[%d,%d] \n",x,y);
-    printf("Server: Attendo connessioni...\n");
-    while (!exitCond)
-    {
-        //Test sul socket: accept non blocca, ma il ciclo while continua
-        //l'esecuzione fino all'arrivo di una connessione.
-        if ((NuovoSocket=accept(DescrittoreSocket,0,0))!=-1)
-        {
-            //Lettura dei dati dal socket (messaggio ricevuto)
-            if ((Quanti=read(NuovoSocket,buffer,sizeof(buffer)))<0){
-                printf("Impossibile leggere il messaggio.\n");
-                ChiudiSocket(NuovoSocket);
-            }
-            else
-            {
-                //Aggiusto la lunghezza...
-                buffer[Quanti]=0;
-                //Elaborazione dati ricevuti
-                int i;
-                if (buffer[2]=='-') {
-                    i=-(string2int(buffer,3,gapArray(buffer)));
-                    if (i==1) return 1;
-                } else {
-                    i=string2int(buffer,2,gapArray(buffer));
-                    if (i==-1) return 1;
-                }
-                
-                if (buffer[0]=='A'|buffer[0]=='a') {
-                    printf("X modificata di %d\n",i);
-                    x=x+i;
-                } else if (buffer[0]=='B'|buffer[0]=='b') {
-                    printf("Y modificata di %d\n",-i);
-                    x=x-i;
-                } else if (buffer[0]=='D'|buffer[0]=='d') {
-                    printf("Y modificata di %d\n",i);
-                    y=y+i;
-                } else if (buffer[0]=='S'|buffer[0]=='s') {
-                    printf("Y modificata di %d\n",-i);
-                    y=y-i;
-                } else {
-                    printf("Server: Imput Error \n");
-                }
-                printf("Server: coordinate attuali:[%d,%d] \n",x,y);
-                
-                
-                //ritorno del messaggio
-                createChar(buffer2,x,y,0);
-                while (buffer2[buf] != '\0') {
-                    printf("%c", buffer2[buf]);
-                    buf++;
-                }
-                printf("\n");
-                if (write(NuovoSocket, buffer2, sizeof(buffer2)) >0 ){
-                    printf("Cordinate inviate al client\n");
-                } else {
-                    printf("Coordinate non inviate");
-                }
-                
-            }
-            //Chiusura del socket temporaneo
-            ChiudiSocket(NuovoSocket);
-        }
-    }
-    //Chiusura del socket
-    ChiudiSocket(DescrittoreSocket);
-    printf("Server: Terminato.\n");
-    return 0;
+	char * str;
+	for ( ; ; ) {
+		connfd = accept(listenfd, 0, 0);
+		printf("%d\n",connfd);
+		lung = read(connfd, buf, sizeof(buf));
+		//	sscanf(str, "%s", str2);		//scansiono parola per parola
+		buf[lung]=0;
+		printf("%s\n",buf);
+	
+	str = strtok(buf," ");
+	while ((str != NULL)&(check(str)!=0)){
+		printf("%s\n",str);
+		str = strtok(NULL," ");
+	}
+	
+		close(connfd);
+	}
 }
